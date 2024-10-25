2df34ca1a68dfb06a2b3cde773051b995068675f
khusnuldwis <khusnuldwi176@gmail.com>
2024-10-25 12:31:08 +0700

    update

--- resources/views/LandingPage.blade.php
+++ resources/views/LandingPage.blade.php
@@ -1,10 +1,14 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mama Laundry - Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
     <style>
         * {
             margin: 0;
@@ -20,16 +24,18 @@
         }
 
         .container {
-            width: 100%;
-            max-width: 1200px;
+            /* width: 100%; */
+            /* max-width: 1200px; */
             margin: auto;
-            padding: 20px;
+            justify-items: center;
+            /* padding: 20px; */
             position: relative;
         }
 
         .login-section {
-            max-width: 400px;
-            margin: 40px 0;
+            /* max-width: 400px; */
+            width: 400px;
+            /* margin: 40px 0; */
         }
 
         .logo {
@@ -39,10 +45,10 @@
             margin-bottom: 30px;
         }
 
-        .logo img {
+        /* .logo img {
             width: 40px;
             height: 40px;
-        }
+        } */
 
         .logo span {
             font-size: 24px;
@@ -64,12 +70,13 @@
             border: none;
             border-bottom: 1px solid #ccc;
             background: transparent;
-            font-size: 16px;
             outline: none;
+            box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+            transition: box-shadow 0.3s ease;
         }
 
         input::placeholder {
-            color: #999;
+            color: #c7cbce;
         }
 
         .login-btn {
@@ -89,16 +96,6 @@
             background-color: #0d1757;
         }
 
-        .decoration {
-            position: fixed;
-            right: 0;
-            top: 0;
-            width: 50%;
-            height: 100%;
-            z-index: -1;
-            overflow: hidden;
-        }
-
         .phone-number {
             position: fixed;
             bottom: 20px;
@@ -109,51 +106,51 @@
             color: #333;
         }
 
-        .illustration {
-            position: absolute;
-            right: 10%;
-            top: 50%;
-            transform: translateY(-50%);
-        }
-
-        /* New styles for additional images */
+        /* Top-right image styling */
         .corner-image-top-right {
             position: absolute;
-            top: 20px;
-            right: 20px;
-            width: 80px; /* Adjust the size as needed */
+            top: 0;
+            right: 0;
+            width: 450px;
+            height: auto;
         }
 
+        /* Bottom-left image styling */
         .corner-image-bottom-left {
             position: absolute;
-            bottom: 20px;
-            left: 20px;
-            width: 80px; /* Adjust the size as needed */
+            bottom: 0;
+            left: 0;
+            width: 450px;
+            height: auto;
         }
 
         @media (max-width: 768px) {
-            .decoration {
-                display: none;
-            }
-            
             .phone-number {
                 position: static;
                 margin-top: 20px;
                 justify-content: center;
             }
+
+            .corner-image-top-right,
+            .corner-image-bottom-left {
+                width: 330px;
+                /* Smaller size for mobile screens */
+            }
         }
     </style>
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="container">
         <div class="login-section">
             <div class="logo">
-                <img src="{{ asset('img/logo-mamalaundry.png') }}" alt="Mama Laundry Logo">
-                <span>Mama Laundry</span>
+                <img src="{{ asset('img/landingPage/logo-mamalaundry.png') }}" style="width: 150px; height: 130px;"
+                    alt="Mama Laundry Logo">
+                {{-- <span><h5>Mama Laundry</h5></span> --}}
             </div>
-            
-            <h2>Login Untuk Akunmu</h2>
-            
+
+            {{-- <h5>Login Untuk Akunmu</h5> --}}
+
             <form>
                 <div class="input-group">
                     <input type="text" placeholder="Username" required>
@@ -166,21 +163,22 @@
         </div>
 
         <div class="phone-number">
-            <img src="/api/placeholder/24/24" alt="Phone Icon">
+            <i class="fas fa-phone"></i>
             <span>+62 858 7596 1416</span>
         </div>
     </div>
 
     <div class="decoration">
-        {{-- <div class="illustration">
-            <img src="/api/placeholder/300/300" alt="Laundry Illustration">
-        </div> --}}
         <!-- Top Right Image -->
-        <img class="corner-image-top-right" src="{{ asset('img/logo-ilustrasi1.png') }}" alt="Top Right Image">
+        <img class="corner-image-top-right" src="{{ asset('img/landingPage/ilustrasi1.png') }}" alt="Top Right Image">
         <!-- Bottom Left Image -->
-        <img class="corner-image-bottom-left" src="{{ asset('img/logo-ilustrasi2.png') }}" alt="Bottom Left Image">
+        <img class="corner-image-bottom-left" src="{{ asset('img/landingPage/ilustrasi2.png') }}"
+            alt="Bottom Left Image">
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
