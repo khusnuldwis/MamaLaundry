72e40f42d3911d4a4fb821c91629032111bf9938
khusnuldwis <khusnuldwi176@gmail.com>
2024-10-25 13:12:06 +0700

    "tampilan order masuk"

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', function () {
     return view('LandingPage');
 });
+Route::get('/orderMasuk', function () {
+    return view('layout/orderMasuk');
+});
