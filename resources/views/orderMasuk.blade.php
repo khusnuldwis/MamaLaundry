64dcf1db7f5c57d124de0b02c759e3a5bdbd8155
khusnuldwis <khusnuldwi176@gmail.com>
2024-11-01 13:55:15 +0700

    "cust"

--- resources/views/orderMasuk.blade.php
+++ resources/views/orderMasuk.blade.php
@@ -4,136 +4,164 @@
 
 @section('content')
 <div class="row">
-    <div class="card card-round">
-        <div class="card-header">
-            <div class="card-head-row">
-                <div class="card-title">Data Pesanan</div>
-                <div class="card-tools">
-                    <a href="#" class="btn btn-primary btn-round">Tambah Layanan</a>
-                </div>
-            </div>
-        </div>
-    </div>
-Order Masuk
-    <!-- Laundry Service Section -->
-    <div class="col-md-6">
-        <div class="card">
+    <div class="col-md-12">
+        <div class="card card-round">
             <div class="card-header">
-                <div class="card-title">Baju</div>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <!-- Laundry Service Cards (Reusable Component) -->
-                    @foreach(['Express', 'Kilat', 'Reguler'] as $service)
-                    <div class="col-4">
-                        <div class="card" data-bs-toggle="{{ $service == 'Reguler' ? 'modal' : '' }}" data-bs-target="{{ $service == 'Reguler' ? '#staticBackdrop' : '' }}">
-                            <div class="card-body text-center">
-                                <img src="https://png.pngtree.com/png-clipart/20211115/original/pngtree-stacked-clothes-exquisite-clothes-cotton-long-sleeve-brand-png-image_6926601.png"
-                                     alt="{{ $service }}" width="80px" class="img-fluid mb-3">
-                                <span><b>{{ $service }}</b></span><br>
-                                <span>Harga: Rp 5000</span><br>
-                                <span>Hari: 3 hari</span>
-                            </div>
-                        </div>
+                <div class="card-head-row">
+                    <div class="card-title">Data Masuk</div>
+                    <div class="card-tools">
+                        <a href="#" class="btn btn-label-success btn-round btn-sm me-2">
+                            <span class="btn-label">
+                                <i class="fa fa-pencil"></i>
+                            </span>
+                            Export
+                        </a>
+                        <a href="#" class="btn btn-label-info btn-round btn-sm">
+                            <span class="btn-label">
+                                <i class="fa fa-print"></i>
+                            </span>
+                            Print
+                        </a>
                     </div>
-                    @endforeach
                 </div>
             </div>
         </div>
-    </div>
+        <div class="card-body">
+            <div class="col-md-12">
+                <div class="table-responsive table-hover table-sales">
+                    <table class="table">
+                        <style>
+                            table {
+                                width: 100%;
+                                table-layout: auto;
+                                border-collapse: collapse;
+                            }
 
-    <!-- Modal for Service Selection -->
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Baju (Reguler)</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="serviceOptions" id="dryClean" value="Cuci Kering">
-                        <label class="form-check-label" for="dryClean">Cuci Kering</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="serviceOptions" id="washIron" value="Cuci Setrika">
-                        <label class="form-check-label" for="washIron">Cuci Setrika</label>
-                    </div>
-                    <div class="px-3 mb-3">
-                        <label for="quantity" class="form-label">Jumlah</label>
-                        <input type="number" class="form-control" id="quantity" aria-describedby="quantityHelp">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                    <button type="button" class="btn btn-primary">Simpan</button>
-                </div>
-            </div>
-        </div>
-    </div>
+                            th,
+                            td {
+                                padding: 8px;
+                                white-space: nowrap;
+                                /* Prevents text from wrapping */
+                                border: 1px solid #ddd;
+                            }
 
-    <!-- Blanket Service Section -->
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-header">
-                <div class="card-title">Selimut</div>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <!-- Reusing Service Cards Component -->
-                    @foreach(['Express', 'Kilat', 'Reguler'] as $service)
-                    <div class="col-4">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDshbRkIeMsncnrfwEN8KpSXR8hrzcEQp_Gg&s"
-                                     alt="{{ $service }}" width="80px" class="img-fluid mb-3">
-                                <span><b>{{ $service }}</b></span><br>
-                                <span>Harga: Rp 5000</span><br>
-                                <span>Hari: 3 hari</span>
-                            </div>
-                        </div>
-                    </div>
-                    @endforeach
+                            /* Optional: Add scroll if the table is too wide */
+                            .table-container {
+                                overflow-x: auto;
+                            }
+                        </style>
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Pelanggan</th>
+                                <th>No HP</th>
+                                <th>Alamat</th>
+                                <th>Layanan</th>
+                                <th>Berat</th>
+                                <th>Tanggal Pemesanan</th>
+                                <th>Tanggal Selesai</th>
+                                <th>Status Barang</th>
+                                <th>Status Pembayaran</th>
+                                <th>Total Pembayaran</th>
+                                <th>Pilihan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Ursa</td>
+                                <td>081234567890</td>
+                                <td>Ds. Jati, Jl. Marak</td>
+                                <td>Cuci Kering</td>
+                                <td>5 Kg</td>
+                                <td>Sep 22, 2024</td>
+                                <td>Sep 25, 2024</td>
+                                <td class="status-barang">
+                                    <select class="form-select status-dropdown"
+                                        onchange="updateDropdownColor(this)">
+                                        <option value="proses" data-color="lightsalmon">Proses
+                                        </option>
+                                        <option value="selesai" data-color="aquamarine">Selesai
+                                        </option>
+                                        <option value="diambil" data-color="darkturquoise">Diambil
+                                        </option>
+                                    </select>
+                                </td>
+                                <td class="status-pembayaran">
+                                    <select class="form-select payment-dropdown"
+                                        onchange="updateDropdownColor(this)">
+                                        <option value="belum_dibayar" data-color="lightcoral">Belum
+                                            Dibayar</option>
+                                        <option value="dibayar" data-color="lightgreen">Dibayar
+                                        </option>
+                                    </select>
+                                </td>
+                                <td>Rp 50,000</td>
+                                <td>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <button class="btn btn-icon btn-round btn-primary btn-sm me-2">
+                                                <i class="fa fa-print"></i>
+                                            </button>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <button class="btn btn-icon btn-round btn-warning btn-sm me-2">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Rasya</td>
+                                <td>081234567890</td>
+                                <td>Ds. Jati, Jl. Beru</td>
+                                <td>Cuci Setrika</td>
+                                <td>10 Pcs</td>
+                                <td>Sep 22, 2024</td>
+                                <td>Sep 25, 2024</td>
+                                <td class="status-barang">
+                                    <select class="form-select status-dropdown"
+                                        onchange="updateDropdownColor(this)">
+                                        <option value="proses" data-color="lightsalmon">Proses
+                                        </option>
+                                        <option value="selesai" data-color="aquamarine">Selesai
+                                        </option>
+                                        <option value="diambil" data-color="darkturquoise">Diambil
+                                        </option>
+                                    </select>
+                                </td>
+                                <td class="status-pembayaran">
+                                    <select class="form-select payment-dropdown"
+                                        onchange="updateDropdownColor(this)">
+                                        <option value="belum_dibayar" data-color="lightcoral">Belum
+                                            Dibayar</option>
+                                        <option value="dibayar" data-color="lightgreen">Dibayar
+                                        </option>
+                                    </select>
+                                </td>
+                                <td>Rp 76,000</td>
+                                <td>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <button class="btn btn-icon btn-round btn-primary btn-sm me-2">
+                                                <i class="fa fa-print"></i>
+                                            </button>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <button class="btn btn-icon btn-round btn-warning btn-sm me-2">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        </div>
-    </div>
-
-    <!-- Charts Section -->
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-header">
-                <div class="card-title">Tristate Chart</div>
-            </div>
-            <div class="card-body">
-                <div class="d-flex justify-content-center p-3">
-                    <div id="sparktristateChart"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-header">
-                <div class="card-title">Discrete Chart</div>
-            </div>
-            <div class="card-body">
-                <div class="d-flex justify-content-center p-3">
-                    <div id="discreteChart"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-header">
-                <div class="card-title">Pie Chart</div>
-            </div>
-            <div class="card-body">
-                <div class="d-flex justify-content-center p-3">
-                    <div id="pieChart"></div>
-                </div>
-            </div>
+            <div id="myChartLegend"></div>
         </div>
     </div>
 </div>
